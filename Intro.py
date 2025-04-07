ac852eb918fb1cf7baef08f842aa0b294d1d690e
tulipsaud <saudtulip4@gmail.com>
2025-04-07 11:47:48 +0545

    Add files via upload

---
+++ Intro.py
@@ -0,0 +1,22 @@
+print("Hello, Welcome to Python!")
+print (9)
+print("Hi World \n")
+print("hello", 6)
+print("Welcome to ", end="*")
+print("New Sen")
+x="Hello"
+y=9
+print(x)
+print(y)
+fifteen = "codingal"
+print(fifteen)
+age = input("enter your age")
+print("\nWow you are", age, ) 
+birthday = input("When is your birthday?")
+print("\nHey", birthday , "\nIts in 2 days!") 
+print("The Python keywords are as follows...\n")
+import keyword
+print(keyword.kwlist)
+name=input("Your Name?")
+print("Hey{0} Nice to meet you {1}". format(name, "dear"))
+print(f"Nice to meet you {name}")
