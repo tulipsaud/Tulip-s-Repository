c386bc41078d2778e5cc9ca7095fb3aaa1d903f1
tulipsaud <saudtulip4@gmail.com>
2025-05-16 11:20:34 +0545

    Add files via upload

---
+++ keywords.py
@@ -0,0 +1,25 @@
+a = input("Enter a word:")
+for i in a:
+    if (i == "A"):
+        print("A found")
+        break
+    else:
+        print("A not found")
+for x in range(10,21):
+    if x % 20 == 0:
+        print("twist")
+    elif x % 15 == 0:
+        pass
+    elif x % 10 == 0:
+        print("fizz")
+    elif x % 5 == 0:
+        print("buzz")
+    else:
+        print(x)
+var = 10
+while var > 0:
+    var = var - 1
+    if var == 6:
+        continue
+    print("Current variable value:", var)
+print("\nGoodbye")
